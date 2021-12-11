99fccca586d9b20e2df315bd442c7b8298e25fd9
Dylan Cleary <G00377815@gmit.ie>
2021-12-11 21:10:42 +0000

    Project Upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+// IMPORTS
 import './App.css';
+import { Component } from 'react';
+import { Header } from './Components/header';
+import { Footer } from './Components/footer';
+import { Content } from './Components/content';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          {/* Navbar Implementation */}
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">BookWorm</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Navbar>
+          <br />
+
+          {/* Routing updated with router-dom V6 */}
+          <Routes>
+            <Route path='/' element={<Content />} exact />
+            <Route path='/read' element={<Footer />} exact />
+            <Route path='/create' element={<Header />} exact />
+          </Routes>
+
+
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Starter Page(Content)</h1>
+            </div>
+        );
+
+
+    }
+}
